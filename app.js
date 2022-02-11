786bffef30ae663a9c9734a01efa46f9de83601f
Stephan Karl <steph@Stephans-MacBook-Pro.local>
2022-02-11 17:36:43 +0000

    small changes

--- app.js
+++ app.js
@@ -8,8 +8,10 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.get('/', (req, res) => {
+    res.send('This Server is running')
+})
+
 app.use('/api/people', peopleRoutes)
 
 app.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}`))
-
-
